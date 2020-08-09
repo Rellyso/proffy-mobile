4a810209093ce96b8b92d61b1e4b361b0be0ddcd
Rellyson <rellysondouglas2015@gmail.com>
2020-08-09 15:58:10 -0300

    Adding Favorite pages

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
+import PageHeader from '../../components/PageHeader';
+import { ScrollView } from 'react-native-gesture-handler';
+import TeacherItem from '../../components/TeacherItem';
 
 import styles from './styles'
-import PageHeader from '../../components/PageHeader';
 
 function Favorites() {
     return (
         <View style={styles.container}>
             <PageHeader title="Meus Proffys favoritos" />
+
+            <ScrollView
+            style={styles.teacherList}
+            contentContainerStyle={{
+                paddingHorizontal: 16,
+                paddingBottom: 16
+            }}
+        >
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+        </ScrollView>
         </View>
     );
 
